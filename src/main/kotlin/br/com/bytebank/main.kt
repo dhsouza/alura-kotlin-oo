022b7db7e58509d8bf576addd9cf5528b3948365
Danilo Hernandes de Souza <danilohernandesdesouza@gmail.com>
2021-05-16 15:43:58 -0300

    utilizando try expressions

--- src/main/kotlin/br/com/bytebank/main.kt
+++ src/main/kotlin/br/com/bytebank/main.kt
@@ -1,29 +1,57 @@
 import br.com.bytebank.modelo.Endereco
 import br.com.bytebank.teste.testaAny
 import java.lang.ClassCastException
+import java.lang.NumberFormatException
 
 fun main() {
     println("início main")
+
+    val entrada: String = "1"
+
+    val valorRecebido: Double? = try {
+        entrada.toDouble()
+    } catch (ex: NumberFormatException) {
+        println("Problema na conversão")
+        ex.printStackTrace()
+        null
+    }
+
+    val valorComTaxa: Double? = if (valorRecebido != null) {
+        valorRecebido + 0.1
+    } else {
+        null
+    }
+
+    if (valorComTaxa != null) {
+        println("valor recebido: $valorComTaxa")
+    } else {
+        println("valor inválido")
+    }
+
     funcao1()
     println("fim main")
 }
 
-fun funcao1(){
+fun funcao1() {
     println("início funcao1")
-    funcao2()
+
+    try {
+        funcao2()
+    } catch (ex: ClassCastException) {
+        ex.printStackTrace()
+        println("ClassCastException thrown")
+    }
+
     println("fim funcao1")
 }
 
 fun funcao2() {
     println("início funcao2")
-    try {
-        for (i in 1..5){
-            println(i)
-            val endereco = Any()
-            endereco as Endereco
-        }
-    } catch (ex: ClassCastException) {
-        println("ClassCastException thrown")
+
+    for (i in 1..5) {
+        println(i)
+        val endereco = Any()
+        endereco as Endereco
     }
 
     println("fim funcao2")
