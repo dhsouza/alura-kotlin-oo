f6e3564d81095d5b57dc10348bed3bb9af4e175c
Danilo Hernandes de Souza <danilohernandesdesouza@gmail.com>
2021-05-16 15:23:50 -0300

    Introdução a exceptions

--- src/main/kotlin/br/com/bytebank/main.kt
+++ src/main/kotlin/br/com/bytebank/main.kt
@@ -1,3 +1,24 @@
+import br.com.bytebank.modelo.Endereco
+import br.com.bytebank.teste.testaAny
 
 fun main() {
+    println("início main")
+    funcao1()
+    println("fim main")
+}
+
+fun funcao1(){
+    println("início funcao1")
+    funcao2()
+    println("fim funcao1")
+}
+
+fun funcao2() {
+    println("início funcao2")
+    for (i in 1..5){
+        println(i)
+        val endereco = Any()
+        endereco as Endereco
+    }
+    println("fim funcao2")
 }
