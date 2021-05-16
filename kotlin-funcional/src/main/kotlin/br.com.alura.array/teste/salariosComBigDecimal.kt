d2ea7c6c27eed416e417441a7e42cdaa40018f3f
Danilo Hernandes de Souza <danilohernandesdesouza@gmail.com>
2021-05-16 19:50:42 -0300

    Array de objetos

---
+++ kotlin-funcional/src/main/kotlin/br.com.alura.array/teste/salariosComBigDecimal.kt
@@ -0,0 +1,32 @@
+package br.com.alura.array.teste
+
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+fun main() {
+    val salarios = bigDecimalArrayOf("1500.55", "2000.00", "5000.00", "10000.00")
+
+    println(salarios.contentToString())
+
+    val aumento = "1.1".toBigDecimal()
+
+    val salariosComAumento: Array<BigDecimal> = salarios.map { salario ->
+        calculaAumentoRelativo(salario, aumento)
+    }.toTypedArray()
+
+    println(salariosComAumento.contentToString())
+}
+
+private fun calculaAumentoRelativo(salario: BigDecimal, aumento: BigDecimal): BigDecimal {
+    return if (salario < "5000".toBigDecimal()) {
+        salario + "500".toBigDecimal()
+    } else {
+        (salario * aumento).setScale(2, RoundingMode.UP)
+    }
+}
+
+fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal> {
+    return Array(valores.size) { i ->
+        valores[i].toBigDecimal()
+    }
+}
