d37d86a9f4266db1ad5dbeed1a6dfa21f7b66de7
Danilo Hernandes de Souza <danilohernandesdesouza@gmail.com>
2021-07-05 08:18:15 -0300

    Expressão lambda e função anônima

--- kotlin-oo/src/main/kotlin/br/com/bytebank/main.kt
+++ kotlin-oo/src/main/kotlin/br/com/bytebank/main.kt
@@ -1,11 +1,26 @@
 package br.com.bytebank
 
 fun main() {
-    val minhaFuncao = ::teste
-    println(minhaFuncao())
 
+    val minhaFuncaoLambda: () -> Unit = {
+        println("Executa como lambda")
+    }
+    println(minhaFuncaoLambda())
+
+    val minhaFuncaoAnonima: () -> Unit = fun() {
+        println("Executa como anonima")
+    }
+    println(minhaFuncaoAnonima())
+}
+
+fun testaTipoFuncaoClasse() {
     val minhaFuncaoClasse: () -> Unit = Teste()
     println(minhaFuncaoClasse())
+}
+
+fun testaTipoFuncaoReferencia() {
+    val minhaFuncao = ::teste
+    println(minhaFuncao())
 }
 
 fun teste() {
